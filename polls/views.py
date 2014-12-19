8ec167e52507f9e0ef458defac90e1ffa7a1dca1
Naffer Reyes <reyesnaffer@gmail.com>
2014-12-19 14:39:00 -0430

    tests agregados

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Question, Choice
 # Create your views here.
@@ -11,12 +12,14 @@ class IndexView(generic.ListView):
     context_object_name = 'lastest_question_list'
 
     def get_queryset(self):
-    	return Question.objects.order_by('-pub_date')[:5]
-
+    	return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = "polls/detail.html"
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
